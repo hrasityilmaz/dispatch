bc81fb10e708a374af1e8c4a1d28f4e059f98ec5
hrasity <hrasity@hotmail.com>
2025-06-28 21:25:18 +0300

    new file:   .gitattributes 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   src/main/java/art/timestop/kafka/dispatch/DispatchApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/art/timestop/kafka/dispatch/DispatchApplicationTests.java

---
+++ src/test/java/art/timestop/kafka/dispatch/DispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package art.timestop.kafka.dispatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

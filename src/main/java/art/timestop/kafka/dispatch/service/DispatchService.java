c077700eee37514e80b1fe3e675cd5595ef6bd46
hrasity <hrasity@hotmail.com>
2025-06-30 09:33:40 +0300

    new file:   src/main/java/art/timestop/kafka/dispatch/message/DispatchPreparing.java 	modified:   src/main/java/art/timestop/kafka/dispatch/service/DispatchService.java

--- src/main/java/art/timestop/kafka/dispatch/service/DispatchService.java
+++ src/main/java/art/timestop/kafka/dispatch/service/DispatchService.java
@@ -3,6 +3,7 @@ package art.timestop.kafka.dispatch.service;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import art.timestop.kafka.dispatch.message.DispatchPreparing;
 import art.timestop.kafka.dispatch.message.OrderCreated;
 import art.timestop.kafka.dispatch.message.OrderDispatched;
 import lombok.RequiredArgsConstructor;
@@ -11,10 +12,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class DispatchService {
 
+    private static final String DISPATCH_TRACKING_TOPIC = "dispatch.tracking";
     private static final String ORDER_DISPATCHED_TOPIC = "order.dispatched";
     private final KafkaTemplate<String, Object> kafkaProducer;
 
     public void process(OrderCreated orderCreated) throws Exception{
+
+        DispatchPreparing dispatchPreparing = DispatchPreparing.builder()
+        .orderId(orderCreated.getOrderId())
+        .build();
+
+        kafkaProducer.send(DISPATCH_TRACKING_TOPIC, dispatchPreparing).get();
+
+
         OrderDispatched orderDispatched = OrderDispatched.builder()
         .orderId(orderCreated.getOrderId())
         .build();
